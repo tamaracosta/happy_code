210b02a1be4caa689c6f8cd4983007808009b0eb
tamaracosta <tamarasantosc@gmail.com>
2023-06-28 18:44:37 +0200

    arquivos padrao

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Banner from './components/Banner';
+import Depoimentos from './components/Depoimentos';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Banner />
+        <Depoimentos           
+        />
+      </header> 
+    </div>
+  );
+}
+
+export default App;
