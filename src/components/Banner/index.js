929ef47d74e26c99c8b31cb7ccaf6c554270d463
tamaracosta <tamarasantosc@gmail.com>
2023-06-28 18:45:25 +0200

    componente Banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from './Banner.module.css';
+import julliFoto from './julli_foto.png';
+import jackieFoto from './jackie_foto.png';
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.titulo}>
+          Olá, Jackie e Julli!
+        </h1>
+
+        <p className={styles.paragrafo}>
+          Feliz aniversárioooooo!<br />
+          Essa é uma homenagem dos alunos e equipe da Campinho!
+        </p>
+      </div>
+
+      <div className={styles.imagens}>
+        <img
+          className={styles.fotoJulli}
+          src={julliFoto}
+          aria-hidden={true}
+          alt="foto Julli"
+        />
+        <img
+          className={styles.fotoJackie}
+          src={jackieFoto}
+          aria-hidden={true}
+          alt="foto Jackie"
+        />
+
+        
+      </div>
+    </div>
+  )
+}
