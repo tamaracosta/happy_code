55fe83ea27482e0cf37f18704fd3094db425368a
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2023-06-30 14:46:23 -0300

    Adiciona função que ordena por ordem alfabética dos nomes o JSON

--- src/components/Depoimentos/index.js
+++ src/components/Depoimentos/index.js
@@ -4,21 +4,32 @@ import depoimentosJson from '../../assets/depoimentos.json'
 import Depoimento from '../Depoimento';
 
 export default function Depoimentos() {
-  
+  const OrdenarJSON = (arquivo, campo) => {
+    return arquivo.sort((a, b) => {
+      if (a[campo] > b[campo]) { 
+        return 1; 
+      }
+      
+      if (b[campo] > a[campo]) { 
+        return -1; 
+      
+      }
+      
+      return 0;
+    })
+  }
+
+  OrdenarJSON(depoimentosJson, "nome");
+
   return (
     <section className={styles.depoimentos}>
-
       {depoimentosJson.map((depoimento, index) => (   
-          
           <Depoimento
             key={index}
             foto={depoimento.imagem}
             nomeDoAluno={depoimento.nome}
             depoimento={depoimento.depoimento}
-          
           />
-          
-        
         )
       )}
 
