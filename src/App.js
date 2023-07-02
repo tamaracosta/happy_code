ba409e41d081bc19d8f26698715c7cd8614135f5
Washington Gomes <wsgomes600@gmail.com>
2023-07-02 15:40:38 -0300

    feat:Criando confetti

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
+
 import Banner from './components/Banner';
 import Depoimentos from './components/Depoimentos';
+import Confetti from './components/Confetti/confetti';
+import ConfettiCDN from './components/Confetti/ConfettiCDN';
+
 
 function App() {
   return (
+    
     <div className="App">
+      
       <header className="App-header">
+        <Confetti />
+        <ConfettiCDN />
         <Banner />
-        <Depoimentos           
-        />
+        <Depoimentos />
+       
       </header> 
+      
     </div>
+    
   );
 }
 
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
-
 import styles from './Banner.module.css';
+
 import julliFoto from './julli_foto.png';
 import jackieFoto from './jackie_foto.png';
 
 export default function Banner() {
+  
   return (
+     
     <div className={styles.banner}>
       <div className={styles.apresentacao}>
         <h1 className={styles.titulo}>
           Olá, Jackie e Julli!
-        </h1>
-
+            </h1>
+               
         <p className={styles.paragrafo}>
           Feliz aniversárioooooo!<br />
           Essa é uma homenagem dos alunos e equipe da Campinho!
         </p>
+     
       </div>
-
+    
       <div className={styles.imagens}>
+      
         <img
           className={styles.fotoJulli}
           src={julliFoto}
@@ -31,9 +35,11 @@ export default function Banner() {
           aria-hidden={true}
           alt="foto Jackie"
         />
-
-        
+      
       </div>
-    </div>
+     </div>
+    
   )
+ 
+  
 }
